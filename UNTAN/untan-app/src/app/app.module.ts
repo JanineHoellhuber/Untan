4fbbc03935397419a8c5dd7d0443e2dff7431313
DominikJonke <56585429+DominikJonke@users.noreply.github.com>
2020-12-02 20:09:44 +0100

    add components (home/comparison/ contact/error)

--- UNTAN/untan-app/src/app/app.module.ts
+++ UNTAN/untan-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ComparisonComponent } from './comparison/comparison.component';
+import { ContactComponent } from './contact/contact.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ComparisonComponent,
+    ContactComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
