3a4c92c04079815d7b4dfc0569354d2eb0a6ddbc
DominikJonke <56585429+DominikJonke@users.noreply.github.com>
2021-04-13 20:20:30 +0200

    fix

--- UNTAN/Angular/untan-app/src/app/interfaces/address.ts
+++
@@ -1,8 +0,0 @@
-export interface Address {
-    adresseId: number;
-    bundesland: string;
-    ort: string;
-    plz: number;
-    strasse: string;
-    hausnummer: string;
-}
--- UNTAN/Angular/untan-app/src/app/interfaces/employee.ts
+++
@@ -1,7 +0,0 @@
-export interface Employee {
-    mitarbeiterId: number;
-    leasing: number;
-    festangestellt: number;
-    auszubildende: number;
-    mitarbeiterzahl: number;
-}
